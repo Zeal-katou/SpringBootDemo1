527f80bff4c80cf07af7fea968a3afacd5ee47bb
譬如北辰 <347914151@qq.com>
2023-05-16 02:49:51 +0800

    控制层,业务逻辑层分离

--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.user.pojo.dto.UserLoginDto;
 import com.example.user.pojo.dto.UserUpdateDto;
 import com.example.user.pojo.entity.User;
 import com.example.user.pojo.vo.UserVO;
+import com.example.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,86 +15,47 @@ import java.util.List;
 @RestController
 public class UserController {
     @Autowired
-    UserMapper mapper;
+    private UserService userService;
 
     @RequestMapping("/add")
     public int add(@RequestBody User user) {
-        System.out.println(user);
-        if (user.getUsername() == null || user.getUsername().equals(" ") ||
-                user.getPassword() == null || user.getPassword().equals(" ")) {
-            // 用户名密码为空
-            return 3;
-        }
-        int row = mapper.selectByAdd(user);
-        if (row == 1) {
-            // 用户名已存在
-            return 2;
-        }
-        mapper.insert(user);
-        // 添加成功
-        return 1;
+        return userService.addUser(user);
     }
 
     @RequestMapping("/delete")
     public int delete(int id) {
-        int row = mapper.deleteById(id);
-        if (row == 1) {
-            // 删除成功
-            return 1;
-        }
-        // 删除失败
-        return 2;
+        return userService.deleteUser(id);
     }
 
     @RequestMapping("/login")
     public int login(@RequestBody UserLoginDto user, HttpSession session) {
-        System.out.println("user = " + user);
-        UserVO u = mapper.selectByUsername(user.getUsername());
-        if (u != null) {
-            if (u.getPassword().equals(user.getPassword())) {
-                // 把当前登录的用户对象保存到当前客户端对应的会话对象里面
-                session.setAttribute("user", u);
-                return 1; // 登录成功
-            }
-            return 2; // 密码错误
-        }
-        return 3; // 用户名不存在
+        return userService.login(user, session);
     }
 
     @RequestMapping("/search")
     public List<User> selectSearch(String username) {
-        List<User> users = mapper.selectSearch(username);
-        System.out.println(users);
-        return users;
+        return userService.selectSearch(username);
     }
 
     @RequestMapping("/select")
     public List<User> selectAll() {
-        return mapper.selectAll();
+        return userService.selectAll();
     }
 
     @RequestMapping("/remSelect")
     public int remSelect(HttpSession session) {
-        UserVO user = (UserVO) session.getAttribute("user");
-        System.out.println(user);
-        return user.getRole();
+        return userService.remSelect(session);
     }
 
     @RequestMapping("/selectById")
     public UserUpdateDto selectById(User user) {
-        UserUpdateDto userDto = mapper.selectById(user.getId());
-        return userDto;
+        return userService.selectById(user);
     }
 
     @RequestMapping("/update")
     public int update(@RequestBody UserUpdateDto userUpdateDto) {
-        System.out.println(userUpdateDto);
-        int row = mapper.update(userUpdateDto);
-        if (row == 1) {
-            return 1;
-        } else {
-            return 2;
-        }
+        return userService.updateUser(userUpdateDto);
     }
 }
+
 
---
+++ src/main/java/com/example/user/service/UserService.java
@@ -0,0 +1,90 @@
+package com.example.user.service;
+
+import com.example.user.mapper.UserMapper;
+import com.example.user.pojo.dto.UserLoginDto;
+import com.example.user.pojo.dto.UserUpdateDto;
+import com.example.user.pojo.entity.User;
+import com.example.user.pojo.vo.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper mapper;
+
+    public int addUser(User user) {
+        if (user.getUsername() == null || user.getUsername().equals(" ") ||
+                user.getPassword() == null || user.getPassword().equals(" ")) {
+            // 用户名密码为空
+            return 3;
+        }
+        int row = mapper.selectByAdd(user);
+        if (row == 1) {
+            // 用户名已存在
+            return 2;
+        }
+        mapper.insert(user);
+        // 添加成功
+        return 1;
+    }
+
+    public int deleteUser(int id) {
+        int row = mapper.deleteById(id);
+        if (row == 1) {
+            // 删除成功
+            return 1;
+        }
+        // 删除失败
+        return 2;
+    }
+
+    public int login(UserLoginDto user, HttpSession session) {
+        System.out.println("user = " + user);
+        UserVO u = mapper.selectByUsername(user.getUsername());
+        if (u != null) {
+            if (u.getPassword().equals(user.getPassword())) {
+                // 把当前登录的用户对象保存到当前客户端对应的会话对象里面
+                session.setAttribute("user", u);
+                return 1; // 登录成功
+            }
+            return 2; // 密码错误
+        }
+        return 3; // 用户名不存在
+    }
+
+    public List<User> selectSearch(String username) {
+        List<User> users = mapper.selectSearch(username);
+        System.out.println(users);
+        return users;
+    }
+
+    public List<User> selectAll() {
+        return mapper.selectAll();
+    }
+
+    public int remSelect(HttpSession session) {
+        UserVO user = (UserVO) session.getAttribute("user");
+        System.out.println(user);
+        return user.getRole();
+    }
+
+    public UserUpdateDto selectById(User user) {
+        UserUpdateDto userDto = mapper.selectById(user.getId());
+        return userDto;
+    }
+
+    public int updateUser(UserUpdateDto userUpdateDto) {
+        System.out.println(userUpdateDto);
+        int row = mapper.update(userUpdateDto);
+        if (row == 1) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
+
